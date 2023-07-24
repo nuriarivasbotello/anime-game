ec77e011f4e70b63d52f021d1ed1c614f7b78366
nuriarivasbotello <nuriarivasbotello@gmail.com>
2023-07-24 19:44:50 +0200

    uwu

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+//Poner las imágenes - div, otro div con cada imagen
+//Hacer que inicie nada más entrar en la web
+//setinterval cada 2 s cambie variable de css, transform translate -100 o 100%
+const carousel = document.getElementById('carousel');
+let index=0;
+const changeImg=()=>{
+    if(index<)
+}
+setInterval (cambiarclase,tiempo)
